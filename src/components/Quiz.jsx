2698d93d0495137343980ddf7bbbc82a8089dad1
thesubodh1 <thesubodh1@gmail.com>
2025-10-20 19:37:21 +0545

    Quiz Completed logic added

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,19 +1,32 @@
 import { useState } from "react";
 
 import QUESTIONS from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png"
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
-  const shuffeledAnswer = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffeledAnswer.sort(() => Math.random() - 0.5);
+  
   //   sort will not return a new array but edit the existing array
+
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((previousAnswers) => {
       return [...previousAnswers, selectedAnswer];
     });
   }
+
+  if (quizIsComplete) {
+    return <div id="summary">
+        <img src={quizCompleteImg} alt="trophy icon" />
+        <h2>Quiz Completed!</h2>
+    </div>
+  }
+
+//   The logic is shifetd below as if quiz is completed than they will give error before the if is executed
+  const shuffeledAnswer = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffeledAnswer.sort(() => Math.random() - 0.5);
   return (
     <div id="quiz">
       <div id="questions">
