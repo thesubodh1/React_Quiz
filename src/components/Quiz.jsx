1122549caeff4aef331fb4099dcd9d14755ea8b2
thesubodh1 <thesubodh1@gmail.com>
2025-10-20 18:54:36 +0545

    Initial version of quiz completed

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,26 @@
 import { useState } from "react";
 
+import QUESTIONS from "../questions.js";
+
 export default function Quiz() {
-    const[userAnswers,setUserAnswers] = useState([]);
-    const activeQuestionIndex = userAnswers.length;
-    return(
-        <p>Curretly active question</p>
-    );
-}  
+  const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((previousAnswers) => {
+      return [...previousAnswers, selectedAnswer];
+    });
+  }
+  return (
+    <div id="questions">
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          <li key={answer} className="answer">
+            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
