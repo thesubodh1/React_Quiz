5cafc25beb97e218bc09091bab237f05399aaf5b
thesubodh1 <thesubodh1@gmail.com>
2025-10-21 12:49:01 +0545

    Infinate handel timeout cleared by adding useCallback

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,37 +1,48 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 import QUESTIONS from "../questions.js";
-import quizCompleteImg from "../assets/quiz-complete.png"
+import quizCompleteImg from "../assets/quiz-complete.png";
 import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
-  
+
   //   sort will not return a new array but edit the existing array
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  function handleSelectAnswer(selectedAnswer) {
-    setUserAnswers((previousAnswers) => {
-      return [...previousAnswers, selectedAnswer];
-    });
-  }
+  const handleSelectAnswer = useCallback(
+    () =>
+      function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers((previousAnswers) => {
+          return [...previousAnswers, selectedAnswer];
+        });
+      },
+    []
+  );
+
+  const handleSkipANswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
 
   if (quizIsComplete) {
-    return <div id="summary">
+    return (
+      <div id="summary">
         <img src={quizCompleteImg} alt="trophy icon" />
         <h2>Quiz Completed!</h2>
-    </div>
+      </div>
+    );
   }
 
-//   The logic is shifetd below as if quiz is completed than they will give error before the if is executed
+  //   The logic is shifetd below as if quiz is completed than they will give error before the if is executed
   const shuffeledAnswer = [...QUESTIONS[activeQuestionIndex].answers];
   shuffeledAnswer.sort(() => Math.random() - 0.5);
   return (
     <div id="quiz">
       <div id="questions">
-        <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)}/>
+        <QuestionTimer timeout={10000} onTimeout={handleSkipANswer} />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffeledAnswer.map((answer) => (
