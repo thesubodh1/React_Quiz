82022df8a36b50348c6cd3c6a194ad159f1c6a19
thesubodh1 <thesubodh1@gmail.com>
2025-10-21 13:29:00 +0545

    Question timer reseted after every question by using key={} trick

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -33,7 +33,11 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="questions">
-        <QuestionTimer timeout={10000} onTimeout={handleSkipANswer}/>
+        <QuestionTimer
+          key={activeQuestionIndex}
+          timeout={10000} 
+          onTimeout={handleSkipANswer}
+        />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffeledAnswer.map((answer) => (
