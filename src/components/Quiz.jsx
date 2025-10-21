27c892016ca592e31f3ae0535633eeb7fd28acd0
thesubodh1 <thesubodh1@gmail.com>
2025-10-21 17:18:31 +0545

    Colour managed for selected answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,33 +1,56 @@
-import { useState,useCallback } from "react";
+import { useState, useCallback } from "react";
 
 import QUESTIONS from "../questions.js";
-import quizCompleteImg from "../assets/quiz-complete.png"
+import quizCompleteImg from "../assets/quiz-complete.png";
 import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
+  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuestionIndex = userAnswers.length;
-  
+  const activeQuestionIndex =
+    answerState === "" ? userAnswers.length : userAnswers.length - 1;
+
   //   sort will not return a new array but edit the existing array
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
-    setUserAnswers((previousAnswers) => {
-      return [...previousAnswers, selectedAnswer];
-    });
-  },[]);
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(selectedAnswer) {
+      setAnswerState("answered");
+      setUserAnswers((previousAnswers) => {
+        return [...previousAnswers, selectedAnswer];
+      });
 
-  const handleSkipANswer = useCallback(() => handleSelectAnswer(null),[handleSelectAnswer])
+      setTimeout(() => {
+        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+
+        setTimeout(() => {
+          setAnswerState("");
+        }, 2000);
+      }, 1000);
+    },
+    [activeQuestionIndex]
+  );
+
+  const handleSkipANswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
 
   if (quizIsComplete) {
-    return <div id="summary">
+    return (
+      <div id="summary">
         <img src={quizCompleteImg} alt="trophy icon" />
         <h2>Quiz Completed!</h2>
-    </div>
+      </div>
+    );
   }
 
-//   The logic is shifetd below as if quiz is completed than they will give error before the if is executed
+  //   The logic is shifetd below as if quiz is completed than they will give error before the if is executed
   const shuffeledAnswer = [...QUESTIONS[activeQuestionIndex].answers];
   shuffeledAnswer.sort(() => Math.random() - 0.5);
   return (
@@ -35,18 +58,38 @@ export default function Quiz() {
       <div id="questions">
         <QuestionTimer
           key={activeQuestionIndex}
-          timeout={10000} 
+          timeout={10000}
           onTimeout={handleSkipANswer}
         />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {shuffeledAnswer.map((answer) => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
+          {shuffeledAnswer.map((answer) => {
+            let cssClass = "";
+
+            const isSelected = userAnswers[userAnswers.length - 1] === answer;
+
+            if (answerState === "answered" && isSelected) {
+              cssClass = "selected";
+            }
+
+            if (
+              (answerState === "correct" || answerState === "wrong") &&
+              isSelected
+            ) {
+              cssClass = answerState;
+            }
+
+            return (
+              <li key={answer} className="answer">
+                <button
+                  onClick={() => handleSelectAnswer(answer)}
+                  className={cssClass}
+                >
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
