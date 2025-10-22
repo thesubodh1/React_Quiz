cba5b202af4c3be90691d0a3e4b26a73756e2031
thesubodh1 <thesubodh1@gmail.com>
2025-10-22 11:50:43 +0545

    answers were being shuffeled while chicking as we are managinc answer state and it was re executing quiz so er used a ref to solve it

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,12 +1,14 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useRef } from "react";
 
 import QUESTIONS from "../questions.js";
 import quizCompleteImg from "../assets/quiz-complete.png";
 import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
+  const shuffeledAnswers = useRef();
   const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
+
   const activeQuestionIndex =
     answerState === "" ? userAnswers.length : userAnswers.length - 1;
 
@@ -51,8 +53,11 @@ export default function Quiz() {
   }
 
   //   The logic is shifetd below as if quiz is completed than they will give error before the if is executed
-  const shuffeledAnswer = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffeledAnswer.sort(() => Math.random() - 0.5);
+  if (!shuffeledAnswers.current) {
+    shuffeledAnswers.current = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffeledAnswers.current.sort(() => Math.random() - 0.5);
+  }
+
   return (
     <div id="quiz">
       <div id="questions">
@@ -63,7 +68,7 @@ export default function Quiz() {
         />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {shuffeledAnswer.map((answer) => {
+          {shuffeledAnswers.current.map((answer) => {
             let cssClass = "";
 
             const isSelected = userAnswers[userAnswers.length - 1] === answer;
