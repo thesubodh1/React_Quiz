7426244da49be127641974703079faa444dcdbbc
thesubodh1 <thesubodh1@gmail.com>
2025-10-30 10:51:06 +0545

    Game Completed

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -2,23 +2,38 @@ import quizCompleteImg from "../assets/quiz-complete.png";
 import QUESTIONS from "../question.js";
 
 export default function Summary({ userAnswers }) {
+  const skippedAnswers = userAnswers.filter((answer) => answer === null);
+  const correctAnswer = userAnswers.filter(
+    (answer, index) => answer === QUESTIONS[index].answers[0]
+  );
+
+  const skippedAnswersShare = Math.round(
+    (skippedAnswers.length / userAnswers.length) * 100
+  );
+
+   const correctAnswersShare = Math.round(
+    (correctAnswer.length / userAnswers.length) * 100
+  );
+
+  const wrongAnswersShare = 100 -skippedAnswersShare - correctAnswersShare
+
   return (
     <div id="summary">
       <img src={quizCompleteImg} alt="trophy icon" />
       <h2>Quiz is Completed</h2>
       <div id="summary-stats">
         <p>
-          <span className="number">10%</span>
+          <span className="number">{skippedAnswersShare}%</span>
           <span className="text">Skipped</span>
         </p>
 
         <p>
-          <span className="number">10%</span>
+          <span className="number">{correctAnswersShare}%</span>
           <span className="text">Answered Correctly</span>
         </p>
 
         <p>
-          <span className="number">10%</span>
+          <span className="number">{wrongAnswersShare}%</span>
           <span className="text">Answered incorrectly</span>
         </p>
       </div>
